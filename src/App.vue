5f7b78be315137becc73697219b1bb8f9445451b
Fenrius <jahnke.kristof2@gmail.com>
2023-08-16 11:40:08 +0200

    add basics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <RouterLink to="/start">Landing</RouterLink> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import LandingView from "../views/LandingView.vue";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
+  {
+    path: "/start",
+    name: "start",
+    component: LandingView,
+  },
   {
     path: "/",
     name: "home",
