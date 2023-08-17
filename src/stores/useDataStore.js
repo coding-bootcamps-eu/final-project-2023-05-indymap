c5db7f6848fdc1716e880f7b4d03aecd93e03574
dominic-reichert <dominicreichert92@gmail.com>
2023-08-17 16:39:04 +0200

    added more pinia actions

--- src/stores/useDataStore.js
+++ src/stores/useDataStore.js
@@ -3,16 +3,25 @@ import { defineStore } from "pinia";
 export const useDataStore = defineStore("state", {
   state() {
     return {
-      stateData: {},
+      stateMaps: {},
+      statePins: {},
       currentMap: "",
+      currentPin: "",
+      newPinLocation: {},
     };
   },
   actions: {
     /* Gets Map Data for one User */
-    fetchApiData(userID) {
+    fetchUserMaps(userID) {
       fetch(`http://localhost:3000/users/${userID}?_embed=maps`)
         .then((response) => response.json())
-        .then((data) => (this.stateData = data));
+        .then((data) => (this.stateMaps = data));
+    },
+
+    fetchMapPins(mapID) {
+      fetch(`http://localhost:3000/maps/${mapID}?_embed=pins`)
+        .then((response) => response.json())
+        .then((data) => (this.statePins = data));
     },
 
     createNewUser(userName) {
@@ -50,6 +59,47 @@ export const useDataStore = defineStore("state", {
 
     deleteMap(mapID) {
       fetch(`http://localhost:3000/maps/${mapID}`, {
+        method: "DELETE",
+      }).then((response) => response.json());
+    },
+
+    createNewPin(header, descripion, geoLocation, mapId) {
+      fetch("http://localhost:3000/pins", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          mapId: mapId,
+          header: header,
+          descripion: descripion,
+          geoLocation: geoLocation,
+          createdAt: new Date(),
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => (this.currentMap = data.id));
+    },
+
+    editPin(pinId, header, descripion, geoLocation, mapId) {
+      fetch(`http://localhost:3000/pins/${pinId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          mapId: mapId,
+          header: header,
+          descripion: descripion,
+          geoLocation: geoLocation,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => (this.currentMap = data.id));
+    },
+
+    deletePin(pinID) {
+      fetch(`http://localhost:3000/pins/${pinID}`, {
         method: "DELETE",
       }).then((response) => response.json());
     },
