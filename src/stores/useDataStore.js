e40c46ea22d4b7a820047062f4c625ae9507f31a
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-08-21 15:34:49 +0200

    Fix: adds reset of newPin to false after saving new pin & fetching of pin data after saving pin

--- src/stores/useDataStore.js
+++ src/stores/useDataStore.js
@@ -5,8 +5,8 @@ export const useDataStore = defineStore("state", {
     return {
       stateMaps: {},
       statePins: {},
-      currentMapId: "",
-      currentPinId: "",
+      currentMapId: "7220e93a-804f-4c9e-880a-8e53e429c1b3",
+      currentPinId: "9623d39d-55f4-434f-bd31-3c776952d7a4",
       newPin: false,
       newPinLocation: {},
     };
