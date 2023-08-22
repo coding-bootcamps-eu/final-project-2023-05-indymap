890b4b5da19b0a702d8a04b92602aed7f8bbae07
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-08-22 15:21:49 +0200

    Fix: input labels are now always visible

--- src/components/CreateMyMap.vue
+++ src/components/CreateMyMap.vue
@@ -3,8 +3,15 @@
     <h1 class="title">Create your map</h1>
     <div class="input-group">
       <i class="fas fa-map-marker-alt"></i>
-      <input type="text" v-model="mapTitle" placeholder="Enter Map Title" />
-      <small class="check"><p v-if="!mapTitle">Please enter a title</p></small>
+      <input
+        type="text"
+        v-model="mapTitle"
+        placeholder="Enter Map Title"
+        id="map-title"
+      />
+      <small class="check"
+        ><label for="map-title">Please enter a title</label></small
+      >
     </div>
     <div class="input-group">
       <i class="fas fa-pen"></i>
@@ -12,9 +19,10 @@
         type="text"
         v-model="mapDescription"
         placeholder="Short Description"
+        id="map-description"
       />
       <small class="check"
-        ><p v-if="!mapDescription">Please enter a description</p></small
+        ><label for="map-description">Please enter a description</label></small
       >
     </div>
     <div class="input-group">
@@ -24,9 +32,10 @@
         v-debounce:500ms="returnCoordinates"
         v-model="mapLocation"
         placeholder="Location"
+        id="map-location"
       />
       <small class="check"
-        ><p v-if="!validCityName">Please enter a valid city name</p></small
+        ><label for="map-location">Please enter a valid city name</label></small
       >
     </div>
     <button
@@ -125,7 +134,7 @@ export default {
   margin-right: 10px;
 }
 
-.check p {
+.check label {
   margin: 0;
   margin-inline-start: 5px;
 }
@@ -134,7 +143,7 @@ export default {
   flex: 1;
   padding: 12px;
   border: 1px solid #a3c6b3;
-  border-radius: 10px;
+  border-radius: 5px;
   font-size: 16px;
   background-color: #ffffff;
   color: #333;
@@ -145,7 +154,7 @@ button {
   color: #fff;
   border: none;
   padding: 15px;
-  border-radius: 10px;
+  border-radius: 5px;
   font-size: 18px;
   width: 10rem;
   cursor: pointer;
